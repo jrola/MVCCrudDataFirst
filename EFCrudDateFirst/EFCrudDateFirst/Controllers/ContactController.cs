10a511b76ae87d56d1d8c015c2f0ad69cfb7db2f
Jack Rola <jrola@precisiondesignstudios.com>
2015-12-29 13:57:49 -0500

    First Commit

---
+++ EFCrudDateFirst/EFCrudDateFirst/Controllers/ContactController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EFCrudDateFirst.Models;
+
+namespace EFCrudDateFirst.Controllers
+{
+    public class ContactController : Controller
+    {
+        private Customers db = new Customers();
+
+        //
+        // GET: /Contact/
+
+        public ActionResult Index()
+        {
+            return View(db.Tables.ToList());
+        }
+
+        //
+        // GET: /Contact/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Table table = db.Tables.Find(id);
+            if (table == null)
+            {
+                return HttpNotFound();
+            }
+            return View(table);
+        }
+
+        //
+        // GET: /Contact/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Contact/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Table table)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Tables.Add(table);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(table);
+        }
+
+        //
+        // GET: /Contact/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Table table = db.Tables.Find(id);
+            if (table == null)
+            {
+                return HttpNotFound();
+            }
+            return View(table);
+        }
+
+        //
+        // POST: /Contact/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Table table)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(table).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(table);
+        }
+
+        //
+        // GET: /Contact/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Table table = db.Tables.Find(id);
+            if (table == null)
+            {
+                return HttpNotFound();
+            }
+            return View(table);
+        }
+
+        //
+        // POST: /Contact/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Table table = db.Tables.Find(id);
+            db.Tables.Remove(table);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ EFCrudDateFirst/EFCrudDateFirst/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EFCrudDateFirst.MvcApplication" Language="C#" %>
